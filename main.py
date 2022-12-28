284769e91ebafc96d6a530272db46d56213146bc
qternel <qternel@gmail.com>
2022-12-28 19:35:22 +0300

    third commit

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-# 123
+from aiogram import Bot, Dispatcher, executor, types
+
+
+TOKEN_API = '5887818155:AAGXj_UKxkRaFp_UcqmAQg3AKd02WYU_9fo'
+
+
+
+bot = Bot(TOKEN_API)
+dp = Dispatcher(bot)
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(text=message.text) # ответить на сообщение text
+
+if __name__ == '__main__':
+    executor.start_polling(dp) # старт бота
