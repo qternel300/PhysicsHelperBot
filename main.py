d98e514254fa40bf1b5ec4d4ea9a1ed4b02dea20
qternel <qternel@gmail.com>
2022-12-30 14:41:31 +0300

    fifth commit

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 from aiogram import Bot, Dispatcher, executor, types
-import os
 from dotenv import load_dotenv
+from os import getenv
 
 load_dotenv()
-TOKEN_API = os.getenv('TOKEN_API')
 
+HELP_COMMAND = """
+<b>/start</b> - <i>начало работы</i>
+<b>/help</b> - <i>начало работы</i>
+<b>/картинка</b> - <i>начало работы</i>
+"""
 
-
+async def on_startup(_):
+    print('Я запустился!')
+TOKEN_API = getenv('TOKEN_API')
 bot = Bot(TOKEN_API)
 dp = Dispatcher(bot)
 
-@dp.message_handler()
-async def echo(message: types.Message):
-    await message.answer(text=message.text) # ответить на сообщение text
+@dp.message_handler(commands=['help'])
+async def help_command(message: types.Message):
+    await bot.send_message(chat_id=message.from_user.id, text=HELP_COMMAND, parse_mode='HTML')
+    await message.delete()
+
+
 
 if __name__ == '__main__':
-    executor.start_polling(dp) # старт бота
+    executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
